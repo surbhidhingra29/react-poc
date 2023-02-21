54555c1b3d067c22fcc6c171f29738cd907a9c7c
surabhidhingra29 <surabhi.dhingra@yash.com>
2023-02-21 12:23:43 +0530

    Day 1 components assignment

---
+++ Day1/my-app/src/components/Animals/Zebra.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Zebra extends React.Component{
+render(){
+  return(
+    <>
+    <h3>Zebra</h3>
+    <p>Zebras are considered to have white coats with brown or black stripes, but they have black skin under their white coats.</p>
+    </>
+  )
+}
+}
+
+export default Zebra;
---
+++ Day1/my-app/src/components/Birds/Swan.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Swan = () => {
+  return (
+    <>
+    <h3>Swan</h3>
+    <p>Swans are the most elegant bird species I know, especially the white ones.</p>
+    </>
+  );
+}
+export default Swan;
